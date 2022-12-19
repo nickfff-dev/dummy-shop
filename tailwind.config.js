2190ceb7e1e58e16b86c3663e0ab01b13bf78d82
unknown <hunchographics@gmail.com>
2022-12-19 17:05:24 +0300

    dummyshop

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx,css}', './components/**/*.{js,ts,jsx,tsx,css}'],
+  theme: {
+    extend: {
+      colors: {
+        "dummygreen": "#0aad0a"
+      }
+    },
+  },
+  plugins: [],
+}
