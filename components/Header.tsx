fd48ec2d7a07b95bcadc94e6e353c22fc5edefd8
unknown <hunchographics@gmail.com>
2022-12-20 17:17:13 +0300

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -76,28 +76,10 @@ const Header = ({ toggleCart, cartCount, cartTotal }: { toggleCart: any, cartCou
         </div>
         
         <div>
-          <button onClick={toggleCart} className="bg-[#f6f7f8] rounded-full px-5 py-2 text-black font-bold flex gap-2 items-center"><svg width="24" height="24" viewBox="0 0 24 24" fill="#343538" xmlns="http://www.w3.org/2000/svg"  color="systemGrayscale70"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.923 4.526 8.5 6H19a1.5 1.5 0 0 1 1.455 1.864l-1.5 6A1.5 1.5 0 0 1 17.5 15h-8a1.5 1.5 0 0 1-1.398-.956L5 6.5a1.5 1.5 0 1 1 0-3h1.5a1.5 1.5 0 0 1 1.423 1.026ZM10 20.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm7 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path></svg><span>{ cartCount}</span></button>
+          <button onClick={toggleCart} className="bg-[#f6f7f8] rounded-full px-5 py-3 text-black font-bold flex gap-2 items-center"><svg width="24" height="24" viewBox="0 0 24 24" fill="#343538" xmlns="http://www.w3.org/2000/svg"  color="systemGrayscale70"><path fillRule="evenodd" clipRule="evenodd" d="M7.923 4.526 8.5 6H19a1.5 1.5 0 0 1 1.455 1.864l-1.5 6A1.5 1.5 0 0 1 17.5 15h-8a1.5 1.5 0 0 1-1.398-.956L5 6.5a1.5 1.5 0 1 1 0-3h1.5a1.5 1.5 0 0 1 1.423 1.026ZM10 20.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm7 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path></svg><span>{ cartCount}</span></button>
        
               </div>
-        {/* <nav className="contents">
-          <ul className=" flex items-center">
         
-          
-            <li className="relative ">
-              <button className="" onClick={toggleCart}>
-                <div className="absolute -top-1 right-0 z-10 bg-dummygreen text-xs text-white font-bold px-1 py-0.5 rounded-sm">{cartCount}</div>
-                <svg className="h-9 lg:h-10 p-2 text-gray-500 svg-inline--fa fa-user fa-w-14 fa-9x"  aria-hidden="true" focusable="false" data-prefix="far" data-icon="shopping-cart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" ><path fill="currentColor" d="M551.991 64H144.28l-8.726-44.608C133.35 8.128 123.478 0 112 0H12C5.373 0 0 5.373 0 12v24c0 6.627 5.373 12 12 12h80.24l69.594 355.701C150.796 415.201 144 430.802 144 448c0 35.346 28.654 64 64 64s64-28.654 64-64a63.681 63.681 0 0 0-8.583-32h145.167a63.681 63.681 0 0 0-8.583 32c0 35.346 28.654 64 64 64 35.346 0 64-28.654 64-64 0-18.136-7.556-34.496-19.676-46.142l1.035-4.757c3.254-14.96-8.142-29.101-23.452-29.101H203.76l-9.39-48h312.405c11.29 0 21.054-7.869 23.452-18.902l45.216-208C578.695 78.139 567.299 64 551.991 64zM208 472c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm256 0c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm23.438-200H184.98l-31.31-160h368.548l-34.78 160z"></path></svg>
-              </button>
-        
-            </li>
-           
-          </ul>
-        </nav> */}
-    
-        {/* <div className=" hidden sm:flex flex-col font-bold">
-          <span className="text-xs text-dummygreen">Your Cart</span>
-          <span>${carT}</span>
-        </div> */}
       </div>
       
       <hr/>
