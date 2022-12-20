3e1c3b0d5024a3704e3a37b22109156bcd9009f6
unknown <hunchographics@gmail.com>
2022-12-20 23:48:28 +0300

    products

--- components/ProductView.tsx
+++ components/ProductView.tsx
@@ -2,7 +2,7 @@ import Product from "./Product"
 
  function ProductView({products,addTocart}:{products:any,addTocart:any}) {
   return (
-    <div className="bg-white">
+    <div className="bg-white -mt-6">
       <div className="mx-auto max-w-2xl py-8 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 <h2 className="text-2xl font-bold tracking-tight text-gray-900">Groceries</h2>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx,css}', './components/**/*.{js,ts,jsx,tsx,css}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,css}', './components/**/*.{js,ts,jsx,tsx,css}', './images/*{png,jpg,jpeg,svg}'],
   theme: {
     extend: {
       colors: {
         "dummygreen": "#0aad0a"
+      },
+      backgroundImage: {
+        "baridi": "url('/images/banner.svg')"
       }
     },
   },
