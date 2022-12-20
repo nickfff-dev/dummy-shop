60d5bc70fa6b923aa0699a41efd3b35e44ea430c
unknown <hunchographics@gmail.com>
2022-12-21 01:34:55 +0300

    products

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -159,7 +159,7 @@ const HomePage = () => {
   return (<>
     <Header toggleCart={toggleCart} cartCount={cartproducts.length} cartTotal={ cartTotal} />
     <div className="grid grid-cols-5 grid-flow-row ">
-      <div className="col-start-1 col-end-2 divide-y border-r border-r-4   ">
+      <div className="col-start-1 col-end-2 divide-y border-r border-r-2 h-screen fixed   ">
       
           <div className="flex flex-col w-full justify-center gap-1 items-center mt-3 row-start-1 row-end-2">
             <Image src={wLogo} width={88} height={88} alt="fr" />
@@ -175,7 +175,7 @@ const HomePage = () => {
             <svg width="12" height="12" viewBox="0 0 24 24" fill="#343538" xmlns="http://www.w3.org/2000/svg"  data-testid="chevron-cta" ><path fillRule="evenodd" clipRule="evenodd" d="m16.879 12-5.94-5.94a1.5 1.5 0 0 1 2.122-2.12l7 7a1.5 1.5 0 0 1 0 2.12l-7 7a1.5 1.5 0 0 1-2.122-2.12L16.88 12Z"></path></svg>
           </div>
           </div>
-          <div className="flex flex-col w-full px-4 pt-5 gap-1">
+          <div className="flex flex-col w-full px-4 pt-5 gap-1 ">
           <button className="flex items-start bg-[#343538] text-white  px-3 py-2 rounded-lg    gap-2  "><span><svg width="24" height="24" viewBox="0 0 24 24" className="focus:fill-[white] fill-[white]" xmlns="http://www.w3.org/2000/svg"  ><path fillRule="evenodd" clipRule="evenodd" d="M12.557 8.393a1.5 1.5 0 0 1-1.114 0L9.5 7.616l-1.936.776a1.5 1.5 0 0 1-.558.108H5a1.5 1.5 0 0 1-1.342-2.17l1-2A1.5 1.5 0 0 1 6 3.5h12a1.5 1.5 0 0 1 1.342.83l1 2A1.5 1.5 0 0 1 19 8.5h-2.002a1.5 1.5 0 0 1-.558-.108l-1.94-.776-1.943.777Zm-8.895 2.926c-.164.32-.164.74-.164 1.581v5.2c0 .84 0 1.26.164 1.581a1.5 1.5 0 0 0 .655.655c.32.164.741.164 1.581.164H7.6c.84 0 1.26 0 1.581-.163a1.5 1.5 0 0 0 .655-.656c.164-.32.164-.74.164-1.581v-.5c0-.56 0-.84.109-1.054a1 1 0 0 1 .437-.437C10.76 16 11.04 16 11.6 16h.8c.56 0 .84 0 1.054.109a1 1 0 0 1 .437.437C14 16.76 14 17.04 14 17.6v.5c0 .84 0 1.26.164 1.581a1.5 1.5 0 0 0 .655.655c.32.164.74.164 1.581.164h1.698c.84 0 1.26 0 1.581-.163a1.5 1.5 0 0 0 .656-.656c.163-.32.163-.74.163-1.581v-5.2c0-.84 0-1.26-.163-1.581a1.5 1.5 0 0 0-.656-.655c-.32-.164-.74-.164-1.581-.164h-12.2c-.84 0-1.26 0-1.58.164a1.5 1.5 0 0 0-.656.655Z"></path></svg></span><span className="font-bold">Shop</span></button>
           <button className="flex items-start   px-3 py-2 rounded-lg   gap-2  "><svg width="24" height="24" viewBox="0 0 24 24" fill="#343538" xmlns="http://www.w3.org/2000/svg"  ><path fillRule="evenodd" clipRule="evenodd" d="M3.95 12.564a1.5 1.5 0 0 0 0 2.122l5.365 5.364a1.5 1.5 0 0 0 2.121 0l7.841-7.842a1.5 1.5 0 0 0 .42-.815l.704-4.24a1.5 1.5 0 0 0-.42-1.306l-1.828-1.829a1.5 1.5 0 0 0-1.306-.419l-4.24.705a1.5 1.5 0 0 0-.815.419l-7.841 7.841Zm13.229-5.69a1.5 1.5 0 1 1-2.122 2.122 1.5 1.5 0 0 1 2.122-2.122Z"></path></svg><span className="font-bold">Deals</span></button>
 
